d2f084376c0a2a6b9089151f6b7dc0f8f5038016
Danny <140586828+itsdannyit@users.noreply.github.com>
2025-01-09 21:47:34 +0700

    Mod  the run.bat

---
+++ Lyricv2thu.cpp
@@ -0,0 +1,49 @@
+#include <windows.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void displayLine(const string& line, int delay)
+{
+    cout << line << endl;
+    Sleep(delay);
+}
+int main()
+{
+    system("color F0");
+    system("chcp 65001 > nul");
+
+
+    system("start run.bat");
+
+    cout <<     "|============================Gthuw============================|" << endl;
+    cout <<     "                        ❤️❤️❤️❤️❤️❤️❤️❤️❤️                       " << endl;
+    Sleep(14000);
+
+    displayLine("500 đổi lấy 1 giờ bao nhiêu đổi lấy 1 giờ bên em", 3000);  // 0:14 to 0:17
+    displayLine("Anh chỉ muốn nhìn thấy em cười vì nó toả sáng luôn cả đêm đen", 3400);  // 0:17 to 0:20
+    displayLine("Bầu trời của 1 gã sầu đời gặp em bỗng chốc hoá thêm màu hồng", 4000);  // 0:20 to 0:24
+    displayLine("Có lẽ em là thứ tuyệt vời mà anh luôn được ôm vào lòng", 3000);  // 0:24 to 0:27
+    displayLine("Nếu tình đắng như ly coffee thì anh sẽ uống không chừa giọt nào", 3000);  // 0:27 to 0:30
+    displayLine("Bởi vì thêm vị soda ý từ môi của em nên thấy ngọt ngào", 3500);  // 0:30 to 0:34
+    displayLine("Em nói em thích dạo phố, thích đi qua những hàng cây xanh", 3500);  // 0:34 to 0:37
+    displayLine("Dẫn em qua những nơi em thích chỉ cần em nắm chặt bàn tay anh", 3500);  // 0:37 to 0:40
+    displayLine("Vẫn sẽ giữ em ở bên mình, không để em yêu thêm ai khác", 3000);  // 0:40 to 0:43
+    displayLine("Bởi vì em chính là báu vật, mà biết bao người luôn muốn khai thác", 4000);  // 0:43 to 0:47
+    displayLine("Anh muốn được cùng em trò chuyện dưới ánh trăng và mặc kệ đêm trôi", 3000);  // 0:47 to 0:50
+    displayLine("Muốn được nói với cả thế giới rằng anh chỉ yêu mình em thôi!!", 3000);  // 0:50 to 0:54
+
+
+    displayLine("Vậy 500 đổi lấy 1 giờ Anh sẻ trân trọng từng giờ bên em", 3500);  // 0:54 to 0:57
+    displayLine("Tình cảm của anh là thật, để anh bật mí cho em xem", 3500);  // 0:57 to 1:00
+    displayLine("Anh không có chơi đồ, nhưng gặp em anh lại cứ phê phê", 3000);  // 1:00 to 1:04
+    displayLine("Đắm chìm trong cơn say, Mỗi khi thấy em là tê tê", 4000);  // 1:04 to 1:07
+    displayLine("Cần gì là chất kích thích và cần gì là hoa có chủ", 3000);  // 1:07 to 1:10
+    displayLine("Không cần lo vì tình yêu anh, dành cho em anh đây có đủ", 3000);  // 1:10 to 1:14
+    displayLine("Call với em xong là ngủ, nhưng cứ nghĩ người đó là em thôi", 3000);  // 1:14 to 1:18
+    displayLine("Nhung nhớ đến từng giờ và ngắm mây mặc kệ về đêm trôi", 3000);  // 1:18 to end
+
+    cout << "\n|========================🎶 End of Song 🎶========================|" << endl;
+    cout << "\n Cred: Danny " << endl;
+    return 0;
+}
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+color F0
+start "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --start-time=0 --stop-time=93 "Z:\Gthuw\Code for T\music\cnt.mp3"
+exit
